6400f7867e674915cb3a9a5f42b910ad6ef66047
sunghyunkang <ksh901016@naver.com>
2020-09-24 14:28:10 +0900

    Map으로 파라미터 추출하기

---
+++ SpringMVCJava/WebContent/WEB-INF/views/result.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: sunghyun
+  Date: 2020-09-24
+  Time: 14:24
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Result</title>
+</head>
+<body>
+
+</body>
+</html>
--- SpringMVCJava/src/kr/co/softcampus/controller/TestController.java
+++ SpringMVCJava/src/kr/co/softcampus/controller/TestController.java
@@ -3,25 +3,22 @@ package kr.co.softcampus.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @Controller
 public class TestController {
 
     @RequestMapping(value = "/test1", method = RequestMethod.GET)
-    public String test1_get(HttpServletRequest request){
-        String data1 = request.getParameter("data1");
-        String data2 = request.getParameter("data2");
-        System.out.println("data1 : " + data1);
-        System.out.println("data2 : " + data2);
-        String[] data3 = request.getParameterValues("data3");
+    public String test1_get(@RequestParam Map<String, String> map){
+        String data1 = map.get("data1");
+        String data2 = map.get("data2");
+        System.out.printf("data1 : %s\n", data1);
+        System.out.printf("data2 : %s\n", data2);
 
-        for(String str1 : data3){
-            System.out.println("data3 : " + str1);
-        }
-        return "test1";
+        return "result";
     }
 
     @RequestMapping(value = "/test2", method = RequestMethod.POST)
